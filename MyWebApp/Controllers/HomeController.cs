afd87c0f0d38ff19d604f74044abf890fa59fdbd
Jamie Corcoran <jamiecorcoran90@gmail.com>
2019-04-21 11:30:33 +0100

    Made cahnges to get images from s3 bucket

--- MyWebApp/Controllers/HomeController.cs
+++ MyWebApp/Controllers/HomeController.cs
@@ -1,18 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Mime;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services.Description;
+using Amazon;
 using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
 
 namespace MyWebApp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
+            ViewBag.ImageDiagram = await GetImageFromByteArray("webappimages", "DiagramAssignment3_S00184418.PNG");
+            ViewBag.ImageHeader = await GetImageFromByteArray("webappimages", "HomeImg.jpg");
             var host = Dns.GetHostEntry(Dns.GetHostName());
             foreach (var ip in host.AddressList)
             {
@@ -36,6 +47,43 @@ namespace MyWebApp.Controllers
             ViewBag.Message = "Address.";
 
             return View();
+        }
+        public async Task<string> GetImageFromByteArray(string folder, string file)
+        {
+            byte[] byteData = await ReadObjectData(folder, file);
+            string imreBase64Data = Convert.ToBase64String(byteData);
+            string imgDataURL = string.Format("data:image/png;base64,{0}", imreBase64Data);
+
+            return imgDataURL;
+        }
+        public async Task<byte[]> ReadObjectData(string key, string fileName)
+        {
+            try
+            {
+                using (var client = new AmazonS3Client(System.Web.Configuration.WebConfigurationManager.AppSettings["AwsKey"].ToString(), System.Web.Configuration.WebConfigurationManager.AppSettings["AwsSecret"].ToString(), RegionEndpoint.EUWest1))
+                {
+                    var request = new GetObjectRequest
+                    {
+                        BucketName = "assignment-bucket-s00184418",
+                        Key = key + "/" + fileName
+                    };
+
+                    using (var getObjectResponse = await client.GetObjectAsync(request))
+                    {
+                        using (var responseStream = getObjectResponse.ResponseStream)
+                        {
+                            var stream = new MemoryStream();
+                            await responseStream.CopyToAsync(stream);
+                            stream.Position = 0;
+                            return stream.ToArray();
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("Read object operation failed.", exception);
+            }
         }
     }
 }
